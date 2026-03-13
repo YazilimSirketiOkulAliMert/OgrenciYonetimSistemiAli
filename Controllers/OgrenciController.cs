8f35f179ea2ce1c9b900fccdc2324dd56238ff01
AliMertTosun <tosunalimert470@gmail.com>
2026-03-13 15:22:49 +0300

    Add files via upload
    değişiklik yapıldı

---
+++ Controllers/OgrenciController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OgrenciYonetimSistemi.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Ogrenci modulu aktif - yeni guncelleme");
+        }
+    }
+}
---
+++ Controllers/OgretmenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OgrenciYonetimSistemi.Controllers
+{
+    public class OgretmenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Ogretmen modulu aktif");
+        }
+    }
+}
